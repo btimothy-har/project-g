4c9b56787da427c660d97cd12ed659d7f7dd7fda
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-10-18 16:11:01 +0800

    Bump version

--- coc_main/cog_coc_main.py
+++ coc_main/cog_coc_main.py
@@ -42,7 +42,7 @@ class ClashOfClansMain(commands.Cog):
     """
 
     __author__ = "bakkutteh"
-    __version__ = "2023.10.13"
+    __version__ = "2023.10.14"
 
     def __init__(self,bot):
         self.bot = bot
