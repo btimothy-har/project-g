e8ea05f09e202a7db3f45add7fd3e3af78b5b614
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-11-22 02:59:25 +0800

    Tweak throttle

--- coc_main/api_client.py
+++ coc_main/api_client.py
@@ -95,20 +95,19 @@ class CustomThrottler(coc.BasicThrottler):
         self.client._api_current_rcvd += 1
     
     async def __aenter__(self):
-        await super().__aenter__()
-        self.increment_sent()
-        # async with self.lock:
+        async with self.lock:
+            self.increment_sent()
             
-        #     last_run = self.last_run
-        #     if last_run:
-        #         difference = pendulum.now() - last_run
-        #         need_to_sleep = (self.sleep_time * 1.3) - difference.total_seconds()
-        #         if need_to_sleep > 0:
-        #             self.client.coc_main_log.debug("Request throttled. Sleeping for %s", need_to_sleep)
-        #             await asyncio.sleep(need_to_sleep)
+            last_run = self.last_run
+            if last_run:
+                difference = pendulum.now() - last_run
+                need_to_sleep = (self.sleep_time * 1.3) - difference.total_seconds()
+                if need_to_sleep > 0:
+                    self.client.coc_main_log.debug("Request throttled. Sleeping for %s", need_to_sleep)
+                    await asyncio.sleep(need_to_sleep)
 
-        #     self.last_run = pendulum.now()
-        #     return self
+            self.last_run = pendulum.now()
+            return self
     
     async def __aexit__(self, exc_type, exc, tb):
         self.increment_rcvd()
