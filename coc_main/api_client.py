bbef438a22fd8461f08720d37d15e8c250b96897
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-11-22 01:19:37 +0800

    Increase rate limit back to default

--- coc_main/api_client.py
+++ coc_main/api_client.py
@@ -435,7 +435,7 @@ class BotClashClient():
         except asyncio.CancelledError:
             pass
 
-    async def api_login(self,rate_limit:int=5):
+    async def api_login(self,rate_limit:int=30):
         try:
             await self.api_login_keys(rate_limit)
         except:
