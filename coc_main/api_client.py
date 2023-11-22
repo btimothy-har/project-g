1ae748d2b16cb90c7ed0f384016b44e969f9669f
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-11-22 21:48:08 +0800

    Log throttle

--- coc_main/api_client.py
+++ coc_main/api_client.py
@@ -91,6 +91,8 @@ class CustomThrottler(coc.BasicThrottler):
         return BotClashClient()
     
     async def __aenter__(self):
+        if not self.limiter.has_capacity():
+            self.client.coc_main_log.warning(f"Throttling request. We've exceeded our limits.")
         await self.limiter.acquire()
         await self.client.api_counter.increment_sent()
         return self
