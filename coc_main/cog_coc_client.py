19db4954d208b29c4c372aee188824c60b42574d
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-11-22 21:05:43 +0800

    Testa dj

--- coc_main/cog_coc_client.py
+++ coc_main/cog_coc_client.py
@@ -431,7 +431,7 @@ class ClashOfClansClient(commands.Cog):
                 + f"\n{'[Connect Time]':<15} {int(mm)}M {int(ss)}S"
                 + f"\n{'[Maintenance]':<15} {self.api_maintenance}"
                 + f"\n{'[API Keys]':<15} " + f"{bot_client.num_keys:,}"
-                + f"\n{'[API Requests]':<15} " + f"{(bot_client.coc.http.key_count * bot_client.coc.http.throttle_limit) - bot_client.coc.http._HTTPClient__lock._value} / {bot_client.coc.http.key_count * bot_client.coc.http.throttle_limit}" + f" (Waiters: {waiters:,})"
+                + f"\n{'[API Requests]':<15} " + f"{(bot_client.coc.http.key_count * bot_client.coc.http.throttle_limit) - bot_client.coc.http._HTTPClient__lock._value:,} / {int(bot_client.coc.http._HTTPClient__throttle.rate_limit):,}" + f" (Waiting: {waiters:,})"
                 + "```",
             inline=False
             )
