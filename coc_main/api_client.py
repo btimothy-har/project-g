acf9f913fc660998844423a96528a3f8eb137819
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-11-24 23:55:00 +0800

    f

--- coc_main/api_client.py
+++ coc_main/api_client.py
@@ -184,9 +184,9 @@ class BotClashClient():
             raise Exception("BotClashClient must be initialized with a bot instance.")
         
         if not self._is_initialized:
-            self.thread_pool = ThreadPoolExecutor(max_workers=1*3)
-            self.read_thread_pool = ThreadPoolExecutor(max_workers=3*3)
-            self.write_thread_pool = ThreadPoolExecutor(max_workers=3*3)
+            self.thread_pool = ThreadPoolExecutor(max_workers=1)
+            self.read_thread_pool = ThreadPoolExecutor(max_workers=3)
+            self.write_thread_pool = ThreadPoolExecutor(max_workers=3)
 
             # LOGGERS
             self.coc_main_log = coc_main_logger
