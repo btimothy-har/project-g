ccd8072d8c94576f447671abafc6d20ddf00e09d
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-11-25 01:44:04 +0800

    test

--- coc_main/api_client.py
+++ coc_main/api_client.py
@@ -305,8 +305,8 @@ class BotClashClient():
             end_of_clan_games
             )
         
-        loop = asyncio.get_running_loop()
-        loop.set_debug(True)
+        # loop = asyncio.get_running_loop()
+        # loop.set_debug(True)
 
         instance._is_initialized = True
         return instance
