d72b5762fa5aa4b9bb85e51cecf912bad8fb2d2f
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-11-25 10:35:46 +0800

    f

--- coc_main/coc_objects/clans/base_clan.py
+++ coc_main/coc_objects/clans/base_clan.py
@@ -664,8 +664,8 @@ class _ClanAttributes():
     async def alliance_members(self) -> List[str]:
         def _get_from_db():
             return [p.tag for p in db_Player.objects(is_member=True,home_clan=self.tag)]
-        players = await bot_client.coc_db.db__clan.find({'is_member':True,'home_clan':self.tag},{'_id':1})
-        return [p['_id'] for p in players]
+        query = await bot_client.coc_db.db__clan.find({'is_member':True,'home_clan':self.tag},{'_id':1})
+        return [p['_id'] async for p in query]
     
     ##################################################
     #####
