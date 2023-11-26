82cdf918afd93d829b91a5c29978b7ea68363f71
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-11-26 12:44:44 +0800

    test

--- coc_main/api_client.py
+++ coc_main/api_client.py
@@ -594,7 +594,8 @@ class BotClashClient():
 
         client = motor.motor_asyncio.AsyncIOMotorClient(
             f'mongodb://{clash_database.get("username")}:{clash_database.get("password")}@localhost:27017/admin',
-            uuidRepresentation="pythonLegacy"
+            uuidRepresentation="pythonLegacy",
+            maxPoolSize=500,
             )
         self.coc_db = client[clash_database.get("dbprimary")]
 
