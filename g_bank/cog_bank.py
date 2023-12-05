ff3fca6a046bda5ec232b5c3260540e9a6e52b6c
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-12-05 19:04:35 +0800

    Bug fix

--- g_bank/cog_bank.py
+++ g_bank/cog_bank.py
@@ -1568,7 +1568,7 @@ class Bank(commands.Cog):
             return await interaction.followup.send("You can't redeem items for bots!")
         
         get_item = await ShopItem.get_by_id(item)
-        inventory = await UserInventory.get_by_user_id(user.id)
+        inventory = await UserInventory(user.id)
 
         if not inventory.has_item(get_item):
             return await interaction.followup.send(f"{user.mention} doesn't have that item.")
