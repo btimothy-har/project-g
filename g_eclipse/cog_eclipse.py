3d6671a4129069826a41a18fd8d0578d796bba0a
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-12-29 17:09:04 +0800

    Test

--- g_eclipse/cog_eclipse.py
+++ g_eclipse/cog_eclipse.py
@@ -228,19 +228,26 @@ class ECLIPSE(commands.Cog):
     def builder_notes_modal(self) -> DiscordModal:
         m = DiscordModal(
             function=self._get_builder_notes,
-            title=f"Builder Notes",
+            title=f"Add New Base",
             )
-        field = discord.ui.TextInput(
-            label="Specify Builder Notes",
+        defensive_cc = discord.ui.TextInput(
+            label="Defensive CC",
+            style=discord.TextStyle.short,
+            required=True
+            )
+        builder_notes = discord.ui.TextInput(
+            label="Builder Notes",
             style=discord.TextStyle.long,
             required=False
-            )
-        m.add_item(field)
+            )        
+        m.add_item(defensive_cc)
+        m.add_item(builder_notes)
         return m
 
     async def _get_builder_notes(self,interaction:discord.Interaction,modal:DiscordModal):
         await interaction.response.defer(ephemeral=True)
-        modal.notes = modal.children[0].value if len(modal.children[0].value) > 0 else "*"
+        modal.defensive_cc = modal.children[0].value
+        modal.notes = modal.children[1].value if len(modal.children[1].value) > 0 else "*"
         modal.stop()
     
     @app_commands.command(name="add-base",
@@ -262,7 +269,6 @@ class ECLIPSE(commands.Cog):
         base_link:str,
         base_image:discord.Attachment,
         base_type:str,
-        defensive_cc:str,
         base_source:str,
         base_builder:Optional[str] = "*"):
 
@@ -278,7 +284,7 @@ class ECLIPSE(commands.Cog):
             source=base_source,
             base_builder=base_builder,
             base_type=base_type,
-            defensive_cc=defensive_cc,
+            defensive_cc=modal.defensive_cc,
             notes=modal.notes,
             image_attachment=base_image)
 
@@ -286,195 +292,3 @@ class ECLIPSE(commands.Cog):
         embed.add_field(name="Base Link",value=new_base.base_link)
 
         return await interaction.followup.send(content="Base Added!",embed=embed,files=[image])
-
-        timeout_embed = await eclipse_embed(context=ctx,message=f"Operation timed out.",success=False,timestamp=pendulum.now())
-
-        def armylink_check(m):
-            msg_check = False
-            if m.author.id == ctx.author.id:
-                if m.channel.id == ctx.channel.id:
-                    msg_check = True
-                elif m.channel.type == ctx.channel.type == discord.ChannelType.private:
-                    msg_check = True
-            if msg_check:
-                try:
-                    link_parse = urllib.parse.urlparse(m.content)
-                    link_action = urllib.parse.parse_qs(link_parse.query)['action'][0]
-
-                    if link_parse.netloc == "link.clashofclans.com" and link_action == "CopyArmy":
-                        check_url = True
-                except:
-                    pass
-                return check_url
-        
-        def response_check(m):
-            if m.author.id == ctx.author.id:
-                if m.channel.id == ctx.channel.id:
-                    return True
-                elif m.channel.type == ctx.channel.type == discord.ChannelType.private:
-                    return True
-                else:
-                    return False                
-
-        msg = await ctx.send("Processing base link...")
-
-        link_parse = urllib.parse.urlparse(base_link)
-        base_id = urllib.parse.quote_plus(urllib.parse.parse_qs(link_parse.query)['id'][0])
-        try:
-            base_townhall = int(base_id.split('TH',1)[1][:2])
-        except:
-            base_townhall = int(base_id.split('TH',1)[1][:1])
-
-        # BASE SOURCE
-        select_view = MultipleChoiceSelectionMenu(ctx)
-        select_view.add_list_item(
-            reference='<:RHBB:1041627382018211900> RH Base Building',
-            label="RH Base Building",
-            emoji="<:RHBB:1041627382018211900>",
-            )
-        select_view.add_list_item(
-            reference='<:BPBB:1043081040090107968> Blueprint Base Building',
-            label="Blueprint Base Building",
-            emoji='<:BPBB:1043081040090107968>',
-            )
-        select_view.add_list_item(
-            reference="<a:aa_AriX:1031773589231374407> Others",
-            label="Others",
-            emoji="<a:aa_AriX:1031773589231374407>",
-            )
-        base_source_embed = await eclipse_embed(
-            context=ctx,
-            title="Add Base -- Step 2/7",
-            message=f"Where is this Base from?")
-        
-        await msg.edit(embed=base_source_embed,view=select_view)
-        timed_out = await select_view.wait()
-
-        if timed_out:
-            return await msg.edit(embed=timeout_embed,view=None)
-        base_source = select_view.return_value
-
-
-        # BASE BUILDER
-        base_builder_embed = await eclipse_embed(
-            context=ctx,
-            title="Add Base -- Step 3/7",
-            message=f"Provide the Name of the Builder. If no Builder is specified, please respond with an asterisk [`*`].")
-        await msg.edit(embed=base_builder_embed,view=None)
-
-        try:
-            builder_response = await ctx.bot.wait_for("message",timeout=60,check=response_check)
-        except asyncio.TimeoutError:
-            return await msg.edit(embed=timeout_embed)
-        else:
-            base_builder = builder_response.content
-            await builder_response.delete()
-
-        #BASE TYPE
-        base_type_view = MultipleChoiceSelectionMenu(ctx)
-        base_type_view.add_list_item(
-            reference='War Base: Anti-3 Star',
-            label="War Base: Anti-3 Star",
-            emoji='<:3_Star:1043063806378651720>',
-            )
-        base_type_view.add_list_item(
-            reference='War Base: Anti-2 Star',
-            label="War Base: Anti-2 Star",
-            emoji='<:Attack_Star:1043063829430542386>',
-            )
-        base_type_view.add_list_item(
-            reference='Legends Base',
-            label="Legends Base",
-            emoji='<:legend_league_star:1043062895652655125>',
-            )
-        base_type_view.add_list_item(
-            reference='Trophy/Farm Base',
-            label="Trophy/Farm Base",
-            emoji='<:HomeTrophies:825589905651400704>',
-            )
-
-        base_type_embed = await eclipse_embed(
-            context=ctx,
-            title="Add Base -- Step 4/7",
-            message=f"Select the type of base this is.")
-        
-        await msg.edit(embed=base_type_embed,view=base_type_view)
-        timed_out = await base_type_view.wait()
-
-        if timed_out:
-            return await msg.edit(embed=timeout_embed,view=None)
-        base_type = base_type_view.return_value
-
-
-        #DEFENSIVE CC
-        defensive_cc_embed = await eclipse_embed(
-            context=ctx,
-            title="Add Base -- Step 5/7",
-            message=f"Provide the Army Link for the Defensive Clan Castle.")
-        await msg.edit(embed=defensive_cc_embed,view=None)
-        try:
-            army_link_response = await ctx.bot.wait_for("message",timeout=60,check=armylink_check)
-        except asyncio.TimeoutError:
-            return await msg.edit(embed=timeout_embed)
-        else:
-            defensive_cc = army_link_response.content
-
-            parsed_cc = ctx.bot.coc_client.parse_army_link(defensive_cc)
-            cc_space = 0
-            for troop in parsed_cc[0]:
-                if troop[0].name in coc.HOME_TROOP_ORDER:
-                    cc_space += (TroopCampSize.get(troop[0].name) * troop[1])
-
-            if cc_space > clan_castle_size[base_townhall][0]:
-                invalid_cc = await eclipse_embed(
-                    context=ctx,
-                    message=f"This Clan Castle composition has more troops than available for this Townhall level."
-                    )
-                return await msg.edit(embed=invalid_cc,view=None)
-            await army_link_response.delete()
-        
-        
-        #BUIDLER NOTES
-        builder_notes_embed = await eclipse_embed(
-            context=ctx,
-            title="Add Notes -- Step 6/7",
-            message=f"Add any Notes from the Builder, if any. If there are no notes, please respond with an asterisk [`*`].")
-        await msg.edit(embed=builder_notes_embed,view=None)
-        
-        try:
-            builder_notes_response = await ctx.bot.wait_for("message",timeout=120,check=response_check)
-        except asyncio.TimeoutError:
-            return await msg.edit(embed=timeout_embed)
-        else:
-            builder_notes = builder_notes_response.content
-            await builder_notes_response.delete()
-        
-
-        ## BASE IMAGE
-        base_image_embed = await eclipse_embed(
-            context=ctx,
-            title="Add Base -- Step 7/7",
-            message=f"Upload the Base Image.")
-        await msg.edit(embed=base_image_embed,view=None)
-
-        try:
-            base_image_response = await ctx.bot.wait_for("message",timeout=60,check=response_check)
-        except asyncio.TimeoutError:
-            return await msg.edit(embed=timeout_embed)
-        else:
-            base_image = base_image_response.attachments[0]
-            await base_image_response.delete()
-
-        new_base = await eWarBase.new_base(
-            base_link=base_link,
-            source=base_source,
-            base_builder=base_builder,
-            base_type=base_type,
-            defensive_cc=defensive_cc,
-            notes=builder_notes,
-            image_attachment=base_image)
-
-        embed,image = await new_base.base_embed()
-        embed.add_field(name="Base Link",value=new_base.base_link)
-
-        return await msg.edit(content="Base Added!",embed=embed,attachments=[image])
