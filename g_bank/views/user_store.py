30c615507c0cc4e8251c12622dc8af73f901d01a
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2023-12-31 22:17:06 +0800

    test

--- g_bank/views/user_store.py
+++ g_bank/views/user_store.py
@@ -236,8 +236,8 @@ class UserStore(DefaultView):
         if menu:
             self.current_item = [i for i in self.store_items if i.id == menu.values[0]][0]
             if self.current_item.type == 'cash' and self.current_item._stock > 0:
-                rand = random.randint(1,20)
-                if rand != 20:
+                rand = random.randint(1,10)
+                if rand != 1:
                     self.current_item._stock = 0
                 else:
                     self.current_item._stock = 1
