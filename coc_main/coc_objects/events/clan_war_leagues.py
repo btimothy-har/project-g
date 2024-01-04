efd0e29cb6b581f65ac6e6c95daa912da40e502e
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2024-01-04 20:21:33 +0800

    lock cwl updates to during

--- coc_main/coc_objects/events/clan_war_leagues.py
+++ coc_main/coc_objects/events/clan_war_leagues.py
@@ -136,22 +136,23 @@ class WarLeagueGroup(AwaitLoader):
 
         wars_by_rounds.sort(key=lambda inner_list: min(war.preparation_start_time.int_timestamp for war in inner_list))
         war_ids_by_rounds = [[war._id for war in round] for round in wars_by_rounds]
-        
-        await bot_client.coc_db.db__war_league_group.update_one(
-            {'_id':group_id},
-            {'$set':{
-                'group_id':group_id,
-                'season':season.id,
-                'league':clan.war_league.name,
-                'number_of_rounds':api_data.number_of_rounds,
-                'clans':[clan.tag for clan in api_data.clans],
-                'rounds':war_ids_by_rounds
-                }},
-            upsert=True
-            )        
-        tasks = [WarLeagueClan.from_api(season.id,group_id,clan) async for clan in AsyncIter(api_data.clans)]
-        await bounded_gather(*tasks,limit=1)
-        
+
+        if api_data.state in ['preparation','inWar']:
+            await bot_client.coc_db.db__war_league_group.update_one(
+                {'_id':group_id},
+                {'$set':{
+                    'group_id':group_id,
+                    'season':season.id,
+                    'league':clan.war_league.name,
+                    'number_of_rounds':api_data.number_of_rounds,
+                    'clans':[clan.tag for clan in api_data.clans],
+                    'rounds':war_ids_by_rounds
+                    }},
+                upsert=True
+                )
+            tasks = [WarLeagueClan.from_api(season.id,group_id,clan) async for clan in AsyncIter(api_data.clans)]
+            await bounded_gather(*tasks,limit=1)
+            
         group = await cls(group_id)
         bot_client.coc_data_log.debug(f"CWL Group: {group_id}"
             + f"\nLeague: {group.league}"
