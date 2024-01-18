046cdcc6aca246ea38eb279968f9db8cb6a0399f
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2024-01-18 19:28:12 +0800

    test

--- coc_commands/views/clan_warlog.py
+++ coc_commands/views/clan_warlog.py
@@ -108,9 +108,9 @@ class ClanWarLog(DefaultView):
             opponent = war.get_opponent(self.clan.tag)
             embed.add_field(
                 name=f"{clan.emoji} {clan.clean_name}\u3000vs\u3000{opponent.clean_name}",
-                value=f"{WarResult.emoji(clan.result)}\u3000{EmojisClash.ATTACK} `{clan.attacks_used:^4}`\u3000{EmojisClash.UNUSEDATTACK} `{clan.unused_attacks:^4}`"
-                    + (f"\n*War Ends <t:{war.end_time.int_timestamp}:R>.*" if war.start_time < pendulum.now() < war.end_time else "")
-                    + (f"\n*War Starts <t:{war.start_time.int_timestamp}:R>.*" if war.start_time > pendulum.now() else "")
+                value=(f"*War Ends <t:{war.end_time.int_timestamp}:R>.*\n" if war.start_time < pendulum.now() < war.end_time else "")
+                    + (f"*War Starts <t:{war.start_time.int_timestamp}:R>.*\n" if war.start_time > pendulum.now() else "")
+                    + f"{WarResult.emoji(clan.result)}\u3000{EmojisClash.ATTACK} `{clan.attacks_used:^4}`\u3000{EmojisClash.UNUSEDATTACK} `{clan.unused_attacks:^4}`"
                     + f"\n{EmojisClash.STAR} `{clan.stars:^8}`\u3000vs\u3000`{opponent.stars:^8}`"
                     + f"\n{EmojisClash.DESTRUCTION} `{clan.destruction:^7.2f}%`\u3000vs\u3000`{opponent.destruction:^7.2f}%`",
                 inline=False
