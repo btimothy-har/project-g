3b56a2e8e6c5fc086844720597230dcc5c3f1168
btimothy <62061780+btimothy-har@users.noreply.github.com>
2024-02-12 00:48:47 +0800

    Bug fix to inventory (#96)

--- g_bank/objects/inventory.py
+++ g_bank/objects/inventory.py
@@ -317,6 +317,8 @@ class UserInventory(AwaitLoader):
         if isinstance(ctx,discord.Interaction):
             user = ctx.user
         
+        elig_items = []
+        
         if len(self.items) > 0:
             elig_items = [i for i in self.items if i.is_user_accessible and i.type in ['basic','cash']]
             elig_items.extend([i for i in self.items if i.type in ['role'] and i.assigns_role])
